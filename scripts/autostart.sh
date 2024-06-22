f4c38d60b17e6cadefe1089ecf960a0a993c9c49
Field User <field@ccom.unh.edu>
2024-06-22 20:46:12 +0000

    adds autostart scripts

---
+++ scripts/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sleep 5
+
+source /opt/ros/noetic/setup.bash
+source /home/field/project11/catkin_ws/devel/setup.bash
+
+export ROS_MASTER_URI=http://mdt:11311
+export ROS_IP=192.168.8.180
+
+
+/usr/bin/tmux new -d -s hailr
+/usr/bin/tmux send-keys -t hailr "rosrun rosmon rosmon --name=hailr hailr vessel.launch" C-m
